8c931b73bee85246dd22fcaaa6c4404663063286
BarthJr <juniior.barth@gmail.com>
2019-02-04 14:07:49 -0200

    Extremely Basic - Sequential

---
+++ Beginner/1001.cpp
@@ -0,0 +1,10 @@
+//https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cin >> a >> b;
+	cout << "X = " << a+b << endl;
+
+	return 0;
+}
