822218425290286b172e1e24d3e6d94e7353a61e
BarthJr <juniior.barth@gmail.com>
2019-02-06 20:43:40 -0200

    Og - Simple implementation

---
+++ Ad-Hoc/1387.cpp
@@ -0,0 +1,11 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1387
+#include<cstdio>
+using namespace std;
+
+int main(){
+
+	int a,b;
+	while(scanf("%d %d",&a,&b) && (a || b))
+		printf("%d\n",a+b);
+	return 0;
+}
