ea941ee0010d0337267f07d30c7cd3c76005bff8
BarthJr <juniior.barth@gmail.com>
2019-02-11 18:00:10 -0200

    Fit or Dont Fit I - Simple Math

---
+++ Mathematics/1240.cpp
@@ -0,0 +1,32 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1240
+#include<cstdio>
+#include<cstring>
+using namespace std;
+
+int main(){
+
+	int n,i,j,tam,tam1,k;
+	char a[31],b[31];
+
+	scanf("%d",&n);
+	for(i=0; i<n; i++){
+		scanf("%s %s",a,b);
+		tam=strlen(a);
+		tam1=strlen(b);
+		if(tam1 > tam)
+			printf("nao encaixa\n");
+		else{
+			j=tam-1;
+			k=tam1-1;
+			while((a[j] == b[k]) && k>=0){
+				j--;
+				k--;
+			}
+			if(k == -1)
+				printf("encaixa\n");
+			else
+				printf("nao encaixa\n");
+		}
+	}
+	return 0;
+}
