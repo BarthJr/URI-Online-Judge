90e76c7891424a2c6f38d6de37006ec3a2588808
BarthJr <juniior.barth@gmail.com>
2019-02-11 15:52:25 -0200

    Back to High School Physics - Phisics, Math

---
+++ Mathematics/1197.cpp
@@ -0,0 +1,12 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1197
+#include<cstdio>
+using namespace std;
+
+int main(){
+
+	int v,t;
+	while(scanf("%d %d",&v,&t) != EOF){
+		printf("%d\n",v*t*2);
+	}
+	return 0;
+}
