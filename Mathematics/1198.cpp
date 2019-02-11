fb6bcefaacebfec001a5ac6f15310dbc018eb4cb
BarthJr <juniior.barth@gmail.com>
2019-02-11 15:55:32 -0200

    Hashmat the Brave Warrior - Simple Math

---
+++ Mathematics/1198.cpp
@@ -0,0 +1,15 @@
+// https://www.urionlinejudge.com.br/judge/en/problems/view/1198
+#include <cstdio>
+using namespace std;
+
+int main(){
+	long long a,b;
+	while (scanf("%lld %lld", &a, &b) != EOF){
+		if (a<=b)
+			printf("%lld\n",b-a);
+		else
+			printf("%lld\n",a-b);
+
+	}
+	return 0;
+}
