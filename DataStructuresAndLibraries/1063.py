821afdc5284faaace20d172a515cc743c2e2bcde
BarthJr <juniior.barth@gmail.com>
2019-02-14 21:18:13 -0200

    Rails Again... Tracing Movements - Stack

--- DataStructuresAndLibraries/1063.py
+++ DataStructuresAndLibraries/1063.py
@@ -3,19 +3,18 @@ def rails_again(rail_a, rail_b):
     rail_a.reverse()
     rail_b.reverse()
     station = []
-    sequence = ''
+    sequence = []
     highest_active_position = len(rail_b) - 1
     while len(rail_a):
         station.append(rail_a.pop())
-        sequence += 'I'
+        sequence.append('I')
         while station and station[-1] == rail_b[highest_active_position]:
             station.pop()
             highest_active_position -= 1
-            sequence += 'R'
-
+            sequence.append('R')
     if station:
-        sequence += ' Impossible'
-    return sequence
+        sequence.append(' Impossible')
+    return ''.join(sequence)
 
 
 def test():
@@ -23,7 +22,7 @@ def test():
     desired_sequence = ['d', 'a', 't', 'e']
     assert rails_again(input_sequence, desired_sequence) == 'IIIRIRRR'
 
-    input_sequence = ['o', 's', 't', 'rail_a', 'p']
+    input_sequence = ['o', 's', 't', 'a', 'p']
     desired_sequence = ['p', 'a', 't', 'o', 's']
     assert rails_again(input_sequence, desired_sequence) == 'IIIIIRRR Impossible'
 
